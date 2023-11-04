1281388c43776db738b598ba37646c958fb29c90
Emil <50961259+EmK530@users.noreply.github.com>
2023-11-04 18:19:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+#pragma warning disable CS0219
+#pragma warning disable CS8321
+#pragma warning disable CS8600
+#pragma warning disable CS8602
+
+bool db = true;
+
+void debug(string input) { Console.WriteLine("\x1b[6;30;44m" + "DEBUG" + "\x1b[0m " + input); }
+void print(string input) { Console.WriteLine("\x1b[6;30;47m" + "INFO" + "\x1b[0m " + input); }
+void warn(string input) { Console.WriteLine("\x1b[6;30;43m" + "WARN" + "\x1b[0m " + input); }
+void error(string input) { Console.WriteLine("\x1b[6;30;41m" + "ERROR" + "\x1b[0m " + input); }
+
+void system(string cmd)
+{
+    Process process = new Process();
+    ProcessStartInfo startInfo = new ProcessStartInfo();
+    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+    startInfo.FileName = "cmd.exe";
+    startInfo.Arguments = "/C "+cmd;
+    process.StartInfo = startInfo;
+    process.Start();
+    process.WaitForExit();
+}
+
+string tempPath = Path.GetTempPath() + "Roblox\\http\\";
+List<string> known = new List<string>();
+
+void thread(string name)
+{
+    byte[] data = File.ReadAllBytes(name);
+    string dataString = Encoding.UTF8.GetString(data[0..(data.Length > 128 ? 128 : data.Length)]);
+    if (dataString.Substring(0,4) != "RBXH")
+    {
+        debug("Ignoring non-RBXH file: "+name);
+        return;
+    }
+    string link = dataString.Substring(dataString.IndexOf("https://")).Split("\x00")[0];
+    print(link);
+}
+
+Console.Title = "BloxDump | Prompt";
+warn("Multithreading is not developed yet.\n");
+Console.WriteLine("Do you want to clear Roblox's cache?");
+Console.WriteLine("Clearing cache will prevent ripping of anything from previous game sessions.");
+Console.WriteLine("Do this if you want to let BloxRip work in real-time while you're playing.");
+Console.Write("Type Y to clear or anything else to proceed: ");
+if (Console.ReadLine().ToLower() == "y")
+{
+    print("\nDeleting Roblox cache...");
+    system("del " + tempPath + "* /q");
+}
+Console.Clear();
+Console.Title = "BloxDump | Idle";
+print("BloxDump started.");
+while (true)
+{
+    int counts = 0;
+    string[] files = Directory.GetFiles(tempPath);
+    int total = files.Length;
+    foreach(string i in files)
+    {
+        string name = i.Split("\\http\\")[1];
+        counts++;
+        Console.Title = "BloxDump | Processing file "+counts+"/"+total+" ("+name+")";
+        if (!name.StartsWith("RBX"))
+        {
+            if (!known.Contains(name))
+            {
+                known.Add(name);
+                thread(i);
+            }
+        } else
+        {
+            warn("Ignoring temporary file: " + name);
+        }
+    }
+    Console.Title = "BloxDump | Idle";
+    print("Ripping loop completed.");
+    Thread.Sleep(10000);
+}
+
+//srgb2lin.convert();
---
+++ srgb2lin.cs
@@ -0,0 +1,39 @@
+﻿// Script made by zuzaratrust and EmK530
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+public static class srgb2lin
+{
+    public static void convert(string outPath)
+    {
+        double srgb2lin(int s)
+        {
+            double lin;
+            if (s <= 0.0404482362771082)
+            {
+                lin = (double)s / 12.92;
+            }
+            else
+            {
+                lin = Math.Pow(((s + 0.055) / 1.055), 2.4);
+            }
+            return lin;
+        }
+        Image<Bgra, byte> img = new Image<Bgra, byte>(outPath);
+        byte[,,] data = (byte[,,])img.Data.Clone();
+        int height = data.GetLength(0);
+        int width = data.GetLength(1);
+        for (int i = 0; i < height * width; i++)
+        {
+            int y = i % height;
+            int x = (int)Math.Floor((double)(i / height));
+            for (int c = 0; c < 3; c++)
+            {
+                data[y, x, c] = (byte)Math.Floor(srgb2lin(data[y, x, c]) / 2058.61501702);
+            }
+        }
+        Image<Rgba, byte> newi = new Image<Rgba, byte>(data);
+        newi.Save(outPath);
+    }
+}
