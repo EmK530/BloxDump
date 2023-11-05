618a9568eb6db926287c13b94554373204b3aaa2
Emil <50961259+EmK530@users.noreply.github.com>
2023-11-05 13:14:29 +0100

    Cleanup

--- srgb2lin.cs
+++ srgb2lin.cs
@@ -1,4 +1,4 @@
-﻿// Script made by zuzaratrust and EmK530
+// Script made by zuzaratrust and EmK530
 
 using Emgu.CV;
 using Emgu.CV.Structure;
@@ -21,19 +21,19 @@ public static class srgb2lin
             return lin;
         }
         Image<Bgra, byte> img = new Image<Bgra, byte>(outPath);
-        byte[,,] data = (byte[,,])img.Data.Clone();
-        int height = data.GetLength(0);
-        int width = data.GetLength(1);
+        int height = img.Data.GetLength(0);
+        int width = img.Data.GetLength(1);
+        int y = -1;
+        int x = 0;
         for (int i = 0; i < height * width; i++)
         {
-            int y = i % height;
-            int x = (int)Math.Floor((double)(i / height));
+            y++;
+            if (y == height){y = 0;x++;}
             for (int c = 0; c < 3; c++)
             {
-                data[y, x, c] = (byte)Math.Floor(srgb2lin(data[y, x, c]) / 2058.61501702);
+                img.Data[y, x, c] = (byte)Math.Floor(srgb2lin(img.Data[y, x, c]) / 2058.61501702);
             }
         }
-        Image<Bgra, byte> newi = new Image<Bgra, byte>(data);
-        newi.Save(outPath);
+        img.Save(outPath);
     }
 }
